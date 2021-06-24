3fed66ece704f7db0510e7eeb7f9d21741c06cbb
SonamAgrahari <sonamagrahari11@gmail.com>
2021-06-24 18:55:58 +0530

    modify scaling of model

--- app/src/main/java/com/thecodework/augmentedreality/MainActivity.kt
+++ app/src/main/java/com/thecodework/augmentedreality/MainActivity.kt
@@ -2,63 +2,76 @@ package com.thecodework.augmentedreality
 
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.ScaleController
 import com.google.ar.sceneform.ux.TransformableNode
 
 class MainActivity : AppCompatActivity() {
-  private var arFragment: ArFragment? = null
-  private var modelRenderable: ModelRenderable? = null
+    private var arFragment: ArFragment? = null
+    private var modelRenderable: ModelRenderable? = null
 
-  @RequiresApi(Build.VERSION_CODES.N)
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-    init()
-  }
-  @RequiresApi(Build.VERSION_CODES.N)
-  private fun init() {
-    arFragment = supportFragmentManager.findFragmentById(R.id.fragment) as ArFragment?
-    setUpModel()
-    setUpPlane()
-  }
-
-
-
-  @RequiresApi(Build.VERSION_CODES.N)
-  private fun setUpModel() {
-    ModelRenderable.builder()
-      .setSource(this, R.raw.astronaut)
-      .build()
-      .thenAccept { renderable: ModelRenderable ->
-        modelRenderable = renderable
-      }
-      .exceptionally { throwable: Throwable? ->
-        Toast.makeText(this@MainActivity, "Model can't be Loaded", Toast.LENGTH_SHORT).show()
-        null
-      }
-  }
-
-  private fun setUpPlane() {
-    arFragment!!.setOnTapArPlaneListener { hitResult, plane, motionEvent ->
-      val anchor = hitResult.createAnchor()
-      val anchorNode = AnchorNode(anchor)
-      anchorNode.setParent(arFragment!!.arSceneView.scene)
-      createModel(anchorNode)
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        init()
     }
-  }
 
-  private fun createModel(anchorNode: AnchorNode) {
-    val node = TransformableNode(arFragment!!.transformationSystem)
-    node.setParent(anchorNode)
-    node.renderable = modelRenderable
-    node.select()
-  }
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun init() {
+        arFragment = supportFragmentManager.findFragmentById(R.id.fragment) as ArFragment?
+        setUpModel()
+        setUpPlane()
+    }
 
 
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun setUpModel() {
+        ModelRenderable.builder()
+            .setSource(this, R.raw.astronaut)
+            .build()
+            .thenAccept { renderable: ModelRenderable ->
+                modelRenderable = renderable
+            }
+            .exceptionally { throwable: Throwable? ->
+                Toast.makeText(this@MainActivity, "Model can't be Loaded", Toast.LENGTH_SHORT)
+                    .show()
+                null
+            }
+    }
+
+    private fun setUpPlane() {
+        arFragment!!.setOnTapArPlaneListener { hitResult, plane, motionEvent ->
+            val anchor = hitResult.createAnchor()
+            val anchorNode = AnchorNode(anchor)
+            anchorNode.setParent(arFragment!!.arSceneView.scene)
+            createModel(anchorNode)
+        }
+    }
+
+    private fun createModel(anchorNode: AnchorNode) {
+        Log.d("msg", "1")
+        val node = TransformableNode(arFragment!!.transformationSystem)
+        node.setParent(anchorNode)
+        node.renderable = modelRenderable    //model creat
+        // node.scaleController.onContinueTransformation()
+        onScale(node)
+        node.select()
+
+
+    }
+
+    private fun onScale(node: TransformableNode) {
+        node.scaleController.maxScale = 1.0f
+        node.scaleController.elasticity = 0.5f
+        node.scaleController.minScale = 0.25f
+    }
 
 }
+
