c02ea20a426b1e836d0ccd60060610ec280f2693
SonamAgrahari <sonamagrahari11@gmail.com>
2022-05-05 15:37:55 +0530

    change textview to button

--- app/src/main/java/com/thecodework/augmentedreality/HomeActivity.kt
+++ app/src/main/java/com/thecodework/augmentedreality/HomeActivity.kt
@@ -3,10 +3,11 @@ package com.thecodework.augmentedreality
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
-    private lateinit var tvView: TextView
+    private lateinit var tvView: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
